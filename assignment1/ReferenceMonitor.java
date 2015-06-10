42533850b01f6605574fe858b4b8483422a9fb38
justwinata <justwinata@gmail.com>
2015-06-10 17:11:47 -0500

    Started print methods

--- assignment1/ReferenceMonitor.java
+++ assignment1/ReferenceMonitor.java
@@ -2,7 +2,7 @@
 
 public class ReferenceMonitor {
 
-	public ReferenceMonitor() {
+	public ReferenceMonitor(InstructionObject io) {
 		ObjectManager om = new ObjectManager();
 
 	}
@@ -18,4 +18,22 @@ public class ReferenceMonitor {
 		}
 	}
 	
+}
+
+public void printExecute (InstructionObject io) {
+	if (io.getType().equals("bad")) {
+		System.out.println("Bad Instruction");
+	}
+	else if (io.getType().equals("write")) {
+		System.out.println(io.getSubject() + " writes value " + io.getValue())
+	}
+}
+
+public void printState (InstructionObject io) {
+	System.out.println("The current state is:");
+	System.out.println("   " + ?.objname + "has value: " + ?.temp);
+	System.out.println("   " + ?.objname + "has value: " + temp);
+	System.out.println("   " + ?.subjname + "has recently read: " + ?.temp);
+	System.out.println("   " + ?.subjname + "has recently read: " + ?.temp);
+	System.out.println();
 }
