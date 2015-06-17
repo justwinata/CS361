041fae30da78ecd7658c1d447a505efcc13fa239
justwinata <justwinata@gmail.com>
2015-06-17 16:14:11 -0500

    Assignment 1 copied over to start off assignment 2

---
+++ assignment2/InstructionObject.java
@@ -0,0 +1,43 @@
+public class InstructionObject {
+	
+		private String instruction;
+		private String subj;
+		private String obj;
+		private int val;
+
+
+		public InstructionObject(String type, String subjname, String objname, int value) {
+			instruction = type;
+			subj = subjname;
+			obj = objname;
+			val = value;
+
+		}
+
+		public InstructionObject(String type) {
+			instruction = type;
+		}
+
+		public InstructionObject(String type, String subjname, String objname) {
+			instruction = type;
+			subj = subjname;
+			obj = objname;
+		}
+
+		public String getType() {
+			return instruction;
+		}
+
+		public String getSubject() {
+			return subj;
+		}
+
+		public String getObject() {
+			return obj;
+		}
+
+		public int getValue() {
+			return val;
+		}
+
+}
---
+++ assignment2/ReferenceMonitor.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class ReferenceMonitor {
+	// object and subject have mapping for name and their values
+	private HashMap<String,Integer> objectValues;
+	private HashMap<String,Integer> subjectValues;
+	// object and subject mapping for name and security levels
+	private HashMap<String, SecurityLevel> objectLevels;
+	private HashMap<String, SecurityLevel> subjectLevels;
+	private InstructionObject inObj;
+
+	public ReferenceMonitor() {
+		ObjectManager om = new ObjectManager();
+		objectValues = new HashMap<String, Integer>();
+	 	subjectValues = new HashMap<String, Integer>();
+	 	objectLevels = new HashMap<String, SecurityLevel>();
+	 	subjectLevels = new HashMap<String, SecurityLevel>();
+	}
+
+	
+	public void addInstruction(InstructionObject io) {
+		this.inObj = io;
+		SecurityLevel one = subjectLevels.get(inObj.getSubject());
+		SecurityLevel two = objectLevels.get(inObj.getObject());
+		if (inObj.getType().toLowerCase().equals("read")) {
+			String s = inObj.getSubject();
+			int v = objectValues.get(inObj.getObject());
+			if (one.dominates(two)) {
+				subjectValues.put(s,v);
+			} else {
+				subjectValues.put(s,0);
+			}
+			printExecute(inObj);
+		} else if (inObj.getType().toLowerCase().equals("write")) {
+			String s = inObj.getObject();
+			int v = inObj.getValue();
+			if (!one.dominates(two)|| one.equals(two)) {
+				objectValues.put(s,v);
+			}
+			printExecute(inObj);
+		} else {
+			printExecute(inObj);
+		}
+			
+	}
+
+	public void printExecute (InstructionObject io) {
+		
+		if (io.getType().toLowerCase().equals("read")) {
+			System.out.println(io.getSubject().toLowerCase() + " reads " + io.getObject().toLowerCase());
+		}
+		else if (io.getType().toLowerCase().equals("write")) {
+			System.out.println(io.getSubject().toLowerCase() + " writes value " + io.getValue() + " to " + io.getObject().toLowerCase());
+		}
+		else  { 
+			System.out.println("Bad Instruction");
+		}
+		printState();
+	}
+
+	
+	public void printState () {
+		System.out.println("The current state is:");
+		System.out.println("   " + "LObj" + " has value: " + objectValues.get("LObj"));
+		System.out.println("   " + "HObj" + " has value: " + objectValues.get("HObj"));
+		System.out.println("   " + "Lyle" + " has recently read: " + subjectValues.get("Lyle"));
+		System.out.println("   " + "Hal" + " has recently read: " + subjectValues.get("Hal"));
+		System.out.println();
+	}
+	
+	
+	public void createSubject(String s, SecurityLevel l) {
+		//s = s.toLowerCase();
+		subjectLevels.put(s,l);
+		subjectValues.put(s, 0);
+	}
+
+	public void createObject(String s, SecurityLevel l) {
+		//s = s.toLowerCase();
+		objectLevels.put(s,l);
+		objectValues.put(s,0);
+	}
+
+	class ObjectManager {
+		
+		public ObjectManager() {
+
+		}
+	}
+
+}
