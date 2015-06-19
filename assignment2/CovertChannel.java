4b35894158ca6e6b9ccee748a8889a9faa343867
Efosa Elaiho <efeelaiho15@utexas.edu>
2015-06-19 16:00:01 -0500

    add Covert Channel

--- assignment2/CovertChannel.java
+++ assignment2/CovertChannel.java
@@ -6,28 +6,30 @@ import java.io.FileOutputStream;
 
 public class CovertChannel {
 
-	private boolean verbose;
-	private SecureSystem sys;
-	private FileOutputStream outfile;
-	private FileOutputStream log;
 
 	public CovertChannel() {
 
 	}
-	
+
 	public static void main(String[] args) {
 		File f;
+		boolean verbose;
+		SecureSystem sys;
 		if (args.length == 2) {
 			verbose = true;
 			f = new File(args[1]);
-			log = new FileOutputStream("log.txt");
 		} else {
 			verbose = false;
 			f = new File(args[0]);
 		}
-		outfile = new FileOutputStream(f.getName() + ".out");
 		sys = new SecureSystem();
 
+		try {
+			FileOutputStream outfile = new FileOutputStream(f.getName() + ".out");
+	 		FileOutputStream log = new FileOutputStream("log.txt");
+	 	} catch(FileNotFoundException e) {
+	 		e.printStackTrace();
+	 	}	
 	}
 
 	
