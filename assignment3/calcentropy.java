ac966b4f85a1819f11c1fce30fc2875b47abf2c4
justwinata <justwinata@gmail.com>
2015-06-24 17:13:49 -0500

    Temporary note added of draft for calculating entropy

---
+++ assignment3/calcentropy.java
@@ -0,0 +1,7 @@
+double entropy = 0.0;
+double total = 0.0;
+for (int i = 0; i < frequencies.length; i++) {
+	log2 = Math.log(frequencies[i]) / Math.log(2);
+	total = total + ( (1 / in) * log2;
+}
+entropy = -1 * total;
