3369e0b7962ec935f91245320a210dff8591c791
Efosa Elaiho <efeelaiho15@utexas.edu>
2015-06-24 16:24:38 -0500

    create File and Scanner Object

--- assignment3/Encoder.java
+++ assignment3/Encoder.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Encoder {
 
 	public static void main(String[] args) {
-		
+		File f = new File(args[0]);
+		try {
+			Scanner sc = new Scanner(f);
+			while (sc.hasNextInt()) {
+				int k;
+				k = sc.nextInt();
+				
+			}
+		} catch(FileNotFoundException e) { 
+			e.printStackTrace(); 
+		}	
 	}
 }
